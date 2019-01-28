8191165a78f8ef21f3440424fa5ef5b039483a4e
Cori Caridine <caridine.corin@gmail.com>
2019-01-28 14:07:23 -0600

    heroku error

--- server.js
+++ server.js
@@ -46,6 +46,5 @@ db.on('open', () => {})
 // ================
 // LISTENER
 // ================
-app.listen(3000, () => {
-  console.log('listening')
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log( 'Listening on port:', PORT))
