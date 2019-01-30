6b67fc4a7222e4c82c9b49aa4e0d62335f897efd
Cori Caridine <caridine.corin@gmail.com>
2019-01-29 18:23:38 -0600

    modal added to index page

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,30 @@
 
 console.log('carousel connected to app.js!')
+// MODAL
+$( () => {
+  // Grab About the Game button
+  const $openBtn = $('#openModal')
+  // Grabb modal element
+  const $modal = $('#modal')
+  // Grab close button
+  const $closeBtn = $('#close')
+
+  // Event handler to open the modal or use .show() >>>$modal.show()
+  const openModal = () => {
+    $modal.css('display', 'block')
+  }
+
+  // Event handler to close the modal or use .hide()>>>>$modal.hide()
+const closeModal = () => {
+  $modal.css('display', 'none')
+}
+
+//Add event listener to Close button
+$closeBtn.on('click', closeModal)
+
+  //Add event listener to About the Game button
+  $openBtn.on('click', openModal)
+})
 
 $(() => {
 // current image counter
